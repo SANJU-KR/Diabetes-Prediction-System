eff0039e78e43b144bd88890c58d3c0ccc82f3b0
SANJU KUMAR <shawkumarsanju5@gmail.com>
2026-02-28 17:28:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -535,7 +535,7 @@ section[data-testid="stSidebar"] div[data-baseweb="select"] span {
 
 /* Dropdown input box background */
 section[data-testid="stSidebar"] div[data-baseweb="select"] > div {
-    background-color: #f1f5f9 !important;   /* light gray */
+    background-color: black !important;   /* light gray */
     color: black !important;
 }
 
@@ -1102,3 +1102,4 @@ if not st.session_state.registered:
     registration_page()
 else:
     prediction_page()
+
